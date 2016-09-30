70f2b0fba9d2b77e34f8f13d806cc48da60c6db5
Angie Smith <avksmit2@Gmail.com>
2016-09-30 10:19:10 -0700

    add a home page and an page to see all stores.

--- app/app.php
+++ app/app.php
@@ -1,7 +1,7 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/***.php";
-    require_once __DIR__."/../src/***.php";
+    require_once __DIR__."/../src/Store.php";
+    require_once __DIR__."/../src/Brand.php";
 
     use Symfony\Component\Debug\Debug;
     Debug::enable();
@@ -13,7 +13,7 @@
 
     $app['debug'] = true;
 
-    $server = 'mysql:host=localhost;dbname=***';
+    $server = 'mysql:host=localhost;dbname=shoes';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
@@ -24,6 +24,16 @@
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render("index.html.twig");
+    });
+
+    $app->get("/stores", function() use ($app) {
+        return $app['twig']->render("stores.html.twig", array('stores' => Store::getAll()));
+    });
+
+    $app->get("/stores_delete_all", function() use ($app) {
+        Store::deleteAll();
+
+        return $app['twig']->render("stores.html.twig", array('stores' => Store::getAll()));
     });
 
     return $app;
--- src/Store.php
+++ src/Store.php
@@ -53,6 +53,7 @@ class Store
     static function deleteAll()
     {
         $GLOBALS['DB']->exec("DELETE FROM stores;");
+        $GLOBALS['DB']->exec("DELETE FROM stores_brands;");
     }
 
     static function find($search_id)
